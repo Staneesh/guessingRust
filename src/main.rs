6fa183d40189fc92150af2e9afc0761b50110057
Staneesh <blkreasons@outlook.com>
2020-06-08 01:37:17 +0200

    added a loop and string->u32 conversion

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,16 +8,29 @@ fn main() {
     let secret_number = rand::thread_rng()
         .gen_range(1, 101);
 
-    println!("Secret number: {}", secret_number);
-   
-    println!("Input your guess:");
+    loop
+    {
+        println!("Input your guess:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => 
+            {
+                println!("You win!");
+                break;
+            }
+        }
+        
+    }
 }
